ecc313b9cb2c008935a6e31fc7dbb0f2840694c1
ad <hi@arnaudiaz.com>
2024-09-15 09:08:59 +0200

    flox added, leveldb, snappy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use clap::Parser;
+
+/// minikeyvalue cli
+#[derive(Parser, Debug)]
+#[clap(
+    version = "0.1.0",
+    author = "Arnau Diaz <arnaudiaz@duck.com>",
+    about = "minikeyvalue cli"
+)]
+struct Cli {
+    /// Sets logging to "debug" level, defaults to "info"
+    #[clap(short, long, global = true)]
+    verbose: bool,
+
+    /// Sets the port to listen on
+    #[clap(short, long, default_value = "3000")]
+    port: u16,
+
+    /// Sets the path to the leveldb
+    #[clap(short, long)]
+    ldb: String,
+
+    /// Calculate and store the MD5 checksum of values
+    #[clap(short, long, default_value = "true")]
+    md5sum: bool,
+}
+
+#[derive(Debug)]
+struct App {
+    port: u16,
+    ldb_path: String,
+    md5sum: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    if cli.verbose {
+        std::env::set_var("RUST_LOG", "debug");
+    } else {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    let app = App {
+        port: cli.port,
+        ldb_path: cli.ldb,
+        md5sum: cli.md5sum,
+    };
+
+    dbg!(&app);
 }
