62da7169a691af471d5eda74655d0fa82de5f766
ad <hi@arnaudiaz.com>
2024-09-15 10:26:50 +0200

    warp

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use log::debug;
+use warp::Filter;
 
 /// minikeyvalue cli
 #[derive(Parser, Debug)]
@@ -32,7 +34,8 @@ struct App {
     md5sum: bool,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     if cli.verbose {
         std::env::set_var("RUST_LOG", "debug");
@@ -48,4 +51,38 @@ fn main() {
     };
 
     dbg!(&app);
+
+    let api = warp_handlers();
+    warp::serve(api).run(([127, 0, 0, 1], app.port)).await;
+
+    Ok(())
+}
+
+fn warp_handlers() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+    let put_record = warp::path::param()
+        .and(warp::put())
+        .and(warp::body::bytes())
+        .and(warp::path::end())
+        .map(handle_put_record);
+
+    let get_record = warp::path::param()
+        .and(warp::get())
+        .and(warp::path::end())
+        .map(handle_get_record);
+
+    put_record.or(get_record)
+}
+
+fn handle_put_record(key: String, value: bytes::Bytes) -> impl warp::Reply {
+    debug!("put_record: key: {}, value: {:?}", key, value);
+    warp::http::Response::builder()
+        .status(warp::http::StatusCode::NOT_IMPLEMENTED)
+        .body(value)
+}
+
+fn handle_get_record(key: String) -> impl warp::Reply {
+    debug!("get_record: key: {}", key);
+    warp::http::Response::builder()
+        .status(warp::http::StatusCode::NOT_IMPLEMENTED)
+        .body("todo")
 }
