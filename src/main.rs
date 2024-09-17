b5f5d75e528e619b64b7b539fcbe9ca4675fab57
ad <hi@arnaudiaz.com>
2024-09-17 12:36:42 +0200

    key cannot be empty

--- src/main.rs
+++ src/main.rs
@@ -148,17 +148,14 @@ async fn handle_put_record(
     } = put_record_context;
 
     info!("put_record: key: {}, value: {:?}", key, value);
-    if content_length.is_none() {
-        debug!("put_record: content_length is none for key: {}", key);
+    if content_length.is_none() || value.is_empty() {
+        debug!(
+            "put_record: content_length is none or value is empty for key: {}",
+            key
+        );
         return Ok(warp::http::Response::builder()
             .status(warp::http::StatusCode::LENGTH_REQUIRED)
-            .body("Content-Length is required".to_string()));
-    }
-
-    if key.is_empty() {
-        return Ok(warp::http::Response::builder()
-            .status(warp::http::StatusCode::LENGTH_REQUIRED)
-            .body("Key cannot be empty".to_string()));
+            .body("Content-Length and data can not be empty".to_string()));
     }
 
     // TODO: handle mutex better for lock_keys and leveldb
@@ -305,12 +302,6 @@ async fn handle_get_record(
     subvolumes: u32,
 ) -> Result<impl warp::Reply, warp::Rejection> {
     info!("get_record: key: {}", key);
-
-    if key.is_empty() {
-        return Ok(warp::http::Response::builder()
-            .status(warp::http::StatusCode::LENGTH_REQUIRED)
-            .body("Key cannot be empty".to_string()));
-    }
 
     let leveldb = leveldb.lock().await;
     let record = match leveldb.get_record_or_default(&key) {
