197360462e55a12d35c30c0996a50d8b9628b514
ad <hi@arnaudiaz.com>
2024-09-17 17:04:45 +0200

    tests back

--- tools/test.py
+++ tools/test.py
@@ -19,114 +19,114 @@ class TestMiniKeyValue(unittest.TestCase):
     return b"http://localhost:3000/swag-" + binascii.hexlify(os.urandom(10))
   
 
-#   def test_one_put(self):
+  def test_one_put(self):
+    key = self.get_fresh_key()
+    r = requests.put(key, data="onyou")
+    self.assertEqual(r.status_code, 201)
+
+  def test_getputdelete(self):
+    key = self.get_fresh_key()
+
+    r = requests.put(key, data="onyou")
+    self.assertEqual(r.status_code, 201)
+
+    r = requests.get(key)
+    self.assertEqual(r.status_code, 200)
+    self.assertEqual(r.text, "onyou")
+
+    r = requests.delete(key)
+    self.assertEqual(r.status_code, 204)
+
+  def test_deleteworks(self):
+    key = self.get_fresh_key()
+
+    r = requests.put(key, data="onyou")
+    self.assertEqual(r.status_code, 201)
+
+    r = requests.delete(key)
+    self.assertEqual(r.status_code, 204)
+
+    r = requests.get(key)
+    self.assertEqual(r.status_code, 404)
+
+  def test_doubledelete(self):
+    key = self.get_fresh_key()
+    r = requests.put(key, data="onyou")
+    self.assertEqual(r.status_code, 201)
+
+    r = requests.delete(key)
+    self.assertEqual(r.status_code, 204)
+
+    r = requests.delete(key)
+    self.assertNotEqual(r.status_code, 204)
+
+  def test_doubleput(self):
+    key = self.get_fresh_key()
+    r = requests.put(key, data="onyou")
+    self.assertEqual(r.status_code, 201)
+
+    r = requests.put(key, data="onyou")
+    self.assertNotEqual(r.status_code, 201)
+
+  def test_doubleputwdelete(self):
+    key = self.get_fresh_key()
+    r = requests.put(key, data="onyou")
+    self.assertEqual(r.status_code, 201)
+
+    r = requests.delete(key)
+    self.assertEqual(r.status_code, 204)
+
+    r = requests.put(key, data="onyou")
+    self.assertEqual(r.status_code, 201)
+
+  def test_10keys(self):
+    keys = [self.get_fresh_key() for _ in range(10)]
+
+    for k in keys:
+      r = requests.put(k, data=hashlib.md5(k).hexdigest())
+      self.assertEqual(r.status_code, 201)
+
+    for k in keys:
+      r = requests.get(k)
+      self.assertEqual(r.status_code, 200)
+      self.assertEqual(r.text, hashlib.md5(k).hexdigest())
+
+    for k in keys:
+      r = requests.delete(k)
+      self.assertEqual(r.status_code, 204)
+
+# TODO
+#   def test_range_request(self):
 #     key = self.get_fresh_key()
 #     r = requests.put(key, data="onyou")
 #     self.assertEqual(r.status_code, 201)
 
-#   def test_getputdelete(self):
-#     key = self.get_fresh_key()
+#     r = requests.get(key, headers={"Range": "bytes=2-5"})
+#     self.assertEqual(r.status_code, 206)
+#     self.assertEqual(r.text, "you")
 
-#     r = requests.put(key, data="onyou")
-#     self.assertEqual(r.status_code, 201)
+  def test_nonexistent_key(self):
+    key = self.get_fresh_key()
+    r = requests.get(key)
+    self.assertEqual(r.status_code, 404)
 
-#     r = requests.get(key)
-#     self.assertEqual(r.status_code, 200)
-#     self.assertEqual(r.text, "onyou")
+  def test_head_request(self):
+    # head not exist
+    key = self.get_fresh_key()
+    r = requests.head(key, allow_redirects=True)
+    self.assertEqual(r.status_code, 404)
+    # no redirect, content length should be zero
+    self.assertEqual(int(r.headers['content-length']), 0)
 
-#     r = requests.delete(key)
-#     self.assertEqual(r.status_code, 204)
-
-#   def test_deleteworks(self):
-#     key = self.get_fresh_key()
-
-#     r = requests.put(key, data="onyou")
-#     self.assertEqual(r.status_code, 201)
-
-#     r = requests.delete(key)
-#     self.assertEqual(r.status_code, 204)
-
-#     r = requests.get(key)
-#     self.assertEqual(r.status_code, 404)
-
-#   def test_doubledelete(self):
-#     key = self.get_fresh_key()
-#     r = requests.put(key, data="onyou")
-#     self.assertEqual(r.status_code, 201)
-
-#     r = requests.delete(key)
-#     self.assertEqual(r.status_code, 204)
-
-#     r = requests.delete(key)
-#     self.assertNotEqual(r.status_code, 204)
-
-#   def test_doubleput(self):
-#     key = self.get_fresh_key()
-#     r = requests.put(key, data="onyou")
-#     self.assertEqual(r.status_code, 201)
-
-#     r = requests.put(key, data="onyou")
-#     self.assertNotEqual(r.status_code, 201)
-
-#   def test_doubleputwdelete(self):
-#     key = self.get_fresh_key()
-#     r = requests.put(key, data="onyou")
-#     self.assertEqual(r.status_code, 201)
-
-#     r = requests.delete(key)
-#     self.assertEqual(r.status_code, 204)
-
-#     r = requests.put(key, data="onyou")
-#     self.assertEqual(r.status_code, 201)
-
-#   def test_10keys(self):
-#     keys = [self.get_fresh_key() for _ in range(10)]
-
-#     for k in keys:
-#       r = requests.put(k, data=hashlib.md5(k).hexdigest())
-#       self.assertEqual(r.status_code, 201)
-
-#     for k in keys:
-#       r = requests.get(k)
-#       self.assertEqual(r.status_code, 200)
-#       self.assertEqual(r.text, hashlib.md5(k).hexdigest())
-
-#     for k in keys:
-#       r = requests.delete(k)
-#       self.assertEqual(r.status_code, 204)
-
-# # TODO
-# #   def test_range_request(self):
-# #     key = self.get_fresh_key()
-# #     r = requests.put(key, data="onyou")
-# #     self.assertEqual(r.status_code, 201)
-
-# #     r = requests.get(key, headers={"Range": "bytes=2-5"})
-# #     self.assertEqual(r.status_code, 206)
-# #     self.assertEqual(r.text, "you")
-
-#   def test_nonexistent_key(self):
-#     key = self.get_fresh_key()
-#     r = requests.get(key)
-#     self.assertEqual(r.status_code, 404)
-
-#   def test_head_request(self):
-#     # head not exist
-#     key = self.get_fresh_key()
-#     r = requests.head(key, allow_redirects=True)
-#     self.assertEqual(r.status_code, 404)
-#     # no redirect, content length should be zero
-#     self.assertEqual(int(r.headers['content-length']), 0)
-
-#     # head exist
-#     key = self.get_fresh_key()
-#     data = "onyou"
-#     r = requests.put(key, data=data)
-#     self.assertEqual(r.status_code, 201)
-#     r = requests.head(key, allow_redirects=True)
-#     self.assertEqual(r.status_code, 200)
-#     # redirect, content length should be size of data
-#     self.assertEqual(int(r.headers['content-length']), len(data))
+    # head exist
+    key = self.get_fresh_key()
+    data = "onyou"
+    r = requests.put(key, data=data)
+    self.assertEqual(r.status_code, 201)
+    r = requests.head(key, allow_redirects=True)
+    self.assertEqual(r.status_code, 200)
+    # redirect, content length should be size of data
+    self.assertEqual(int(r.headers['content-length']), len(data))
 
   # TODO this test passes locally but fails on github actions because of the size. Github sucks.
   # def test_large_key(self):
@@ -187,19 +187,19 @@ class TestMiniKeyValue(unittest.TestCase):
 #     self.assertEqual(r.status_code, 200)
 #     self.assertEqual(r.json(), {"next": "", "keys": keys[limit:]})
 
-  # def test_noemptykey(self):
-  #   key = self.get_fresh_key()
-  #   r = requests.put(key, data="")
-  #   self.assertEqual(r.status_code, 411)
+  def test_noemptykey(self):
+    key = self.get_fresh_key()
+    r = requests.put(key, data="")
+    self.assertEqual(r.status_code, 411)
 
-  # def test_content_hash(self):
-  #   for _ in range(10):
-  #     key = self.get_fresh_key()
-  #     r = requests.put(key, data=key)
-  #     self.assertEqual(r.status_code, 201)
+  def test_content_hash(self):
+    for _ in range(10):
+      key = self.get_fresh_key()
+      r = requests.put(key, data=key)
+      self.assertEqual(r.status_code, 201)
 
-  #     r = requests.head(key, allow_redirects=False)
-  #     self.assertEqual(r.headers['Content-Md5'], hashlib.md5(key).hexdigest())
+      r = requests.head(key, allow_redirects=False)
+      self.assertEqual(r.headers['Content-Md5'], hashlib.md5(key).hexdigest())
 
 if __name__ == '__main__':
   # wait for servers
