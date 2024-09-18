200f0de57964d430f1658f67a1667294153c5885
ad <hi@arnaudiaz.com>
2024-09-18 11:09:58 +0200

    test

---
+++ tools/bringup-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kill $(pgrep -f nginx)
+
+PORT=3001 ./volume /tmp/volume1/ &
+PORT=3002 ./volume /tmp/volume2/ &
+PORT=3003 ./volume /tmp/volume3/ &
+PORT=3004 ./volume /tmp/volume4/ &
+PORT=3005 ./volume /tmp/volume5/ &
+
+cargo run -- --leveldb-path /tmp/indexdb/ --volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 
